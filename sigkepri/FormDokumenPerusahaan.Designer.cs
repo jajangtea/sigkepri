96c2ed62ccb35e080278de965e075042c1579727
jajangtea <za2ng2509@gmail.com>
2018-05-08 08:41:48 +0700

    design form dokumen perusahaan

--- sigkepri/FormDokumenPerusahaan.Designer.cs
+++ sigkepri/FormDokumenPerusahaan.Designer.cs
@@ -28,11 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDokumenPerusahaan));
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.NamaDokumenTextBox = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label3
+            // 
+            this.Label3.Font = new System.Drawing.Font("Segoe UI Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label3.ForeColor = System.Drawing.Color.White;
+            this.Label3.Location = new System.Drawing.Point(14, 221);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(254, 46);
+            this.Label3.TabIndex = 17;
+            this.Label3.Text = "Form berikut digunakan untuk mengelola data perizinan yang dilayani oleh dinas pe" +
+    "rtambangan provinsi Kepri.";
+            // 
+            // Panel1
+            // 
+            this.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(104)))), ((int)(((byte)(116)))));
+            this.Panel1.Controls.Add(this.Label3);
+            this.Panel1.Controls.Add(this.btnHapus);
+            this.Panel1.Controls.Add(this.btnTambah);
+            this.Panel1.Controls.Add(this.btnEdit);
+            this.Panel1.Controls.Add(this.PictureBox1);
+            this.Panel1.Controls.Add(this.Label2);
+            this.Panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Panel1.Location = new System.Drawing.Point(0, 0);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(269, 387);
+            this.Panel1.TabIndex = 41;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.BackColor = System.Drawing.Color.Yellow;
+            this.btnHapus.FlatAppearance.BorderSize = 0;
+            this.btnHapus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHapus.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHapus.ForeColor = System.Drawing.Color.Navy;
+            this.btnHapus.Location = new System.Drawing.Point(43, 343);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(182, 30);
+            this.btnHapus.TabIndex = 1;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = false;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.BackColor = System.Drawing.Color.Yellow;
+            this.btnTambah.FlatAppearance.BorderSize = 0;
+            this.btnTambah.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTambah.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTambah.ForeColor = System.Drawing.Color.Navy;
+            this.btnTambah.Location = new System.Drawing.Point(43, 275);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(182, 30);
+            this.btnTambah.TabIndex = 0;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.Yellow;
+            this.btnEdit.FlatAppearance.BorderSize = 0;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.ForeColor = System.Drawing.Color.Navy;
+            this.btnEdit.Location = new System.Drawing.Point(43, 309);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(182, 30);
+            this.btnEdit.TabIndex = 0;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.ForeColor = System.Drawing.Color.White;
+            this.Label2.Location = new System.Drawing.Point(67, 33);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(135, 25);
+            this.Label2.TabIndex = 13;
+            this.Label2.Text = "Data Dokumen";
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.BackColor = System.Drawing.Color.Crimson;
+            this.btnSimpan.FlatAppearance.BorderSize = 0;
+            this.btnSimpan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSimpan.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSimpan.ForeColor = System.Drawing.Color.White;
+            this.btnSimpan.Location = new System.Drawing.Point(279, 349);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(371, 30);
+            this.btnSimpan.TabIndex = 39;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = false;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(104)))), ((int)(((byte)(116)))));
+            this.Label1.Location = new System.Drawing.Point(279, 7);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(182, 25);
+            this.Label1.TabIndex = 40;
+            this.Label1.Text = "Input Data Dokumen";
+            // 
+            // NamaDokumenTextBox
+            // 
+            this.NamaDokumenTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.NamaDokumenTextBox.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NamaDokumenTextBox.ForeColor = System.Drawing.Color.Black;
+            this.NamaDokumenTextBox.HintForeColor = System.Drawing.Color.Black;
+            this.NamaDokumenTextBox.HintText = "Nama Dokumen";
+            this.NamaDokumenTextBox.isPassword = false;
+            this.NamaDokumenTextBox.LineFocusedColor = System.Drawing.Color.Crimson;
+            this.NamaDokumenTextBox.LineIdleColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(104)))), ((int)(((byte)(116)))));
+            this.NamaDokumenTextBox.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(104)))), ((int)(((byte)(116)))));
+            this.NamaDokumenTextBox.LineThickness = 2;
+            this.NamaDokumenTextBox.Location = new System.Drawing.Point(280, 36);
+            this.NamaDokumenTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.NamaDokumenTextBox.Name = "NamaDokumenTextBox";
+            this.NamaDokumenTextBox.Size = new System.Drawing.Size(369, 33);
+            this.NamaDokumenTextBox.TabIndex = 37;
+            this.NamaDokumenTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // dgvList
+            // 
+            this.dgvList.AllowUserToAddRows = false;
+            this.dgvList.AllowUserToDeleteRows = false;
+            this.dgvList.BackgroundColor = System.Drawing.Color.White;
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Location = new System.Drawing.Point(279, 76);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.ReadOnly = true;
+            this.dgvList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvList.Size = new System.Drawing.Size(371, 266);
+            this.dgvList.TabIndex = 38;
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox1.Image")));
+            this.PictureBox1.Location = new System.Drawing.Point(64, 76);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(140, 123);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.PictureBox1.TabIndex = 0;
+            this.PictureBox1.TabStop = false;
+            // 
+            // FormDokumenPerusahaan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(651, 387);
+            this.Controls.Add(this.Panel1);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.NamaDokumenTextBox);
+            this.Controls.Add(this.dgvList);
+            this.Name = "FormDokumenPerusahaan";
             this.Text = "FormDokumenPerusahaan";
+            this.Panel1.ResumeLayout(false);
+            this.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Panel Panel1;
+        internal System.Windows.Forms.Button btnHapus;
+        internal System.Windows.Forms.Button btnTambah;
+        internal System.Windows.Forms.Button btnEdit;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Button btnSimpan;
+        internal System.Windows.Forms.Label Label1;
+        internal Bunifu.Framework.UI.BunifuMaterialTextbox NamaDokumenTextBox;
+        internal System.Windows.Forms.DataGridView dgvList;
     }
 }
